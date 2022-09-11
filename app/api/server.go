bfa2b80ea345877a2d7df5aa8642be64a3eefe87
Alexander Dobromilskiy <alexdob.twist@gmail.com>
2022-09-11 18:38:28 +0300

    add types for mongodb

--- app/api/server.go
+++ app/api/server.go
@@ -1,8 +1,7 @@
 package api
 
 import (
-	"fmt"
-	"io"
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -24,29 +23,55 @@ func (s *Server) Run() {
 
 	err := s.httpServer.ListenAndServe()
 	if err != nil {
-		log.Printf("[ERROR] http server terminated, %s", err)
+		log.Printf("[ERROR] http server terminated: %s", err)
 	}
 }
 
 func (s *Server) routes() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api/totalmatches", s.getInfo)
+	mux.HandleFunc("/api/ffa", s.getFAAmatches)
 
 	return mux
 }
 
-func (s *Server) getInfo(w http.ResponseWriter, r *http.Request) {
+func (s *Server) getFAAmatches(w http.ResponseWriter, r *http.Request) {
 	col := s.Database.Collection("matches")
+	ctx := r.Context()
+	var matches []Match
 
-	totalGames, err := col.CountDocuments(r.Context(), bson.M{"type": "FFA"})
+	// totalGames, err := col.CountDocuments(r.Context(), bson.M{"type": "FFA"})
+	cur, err := col.Find(ctx, bson.M{"type": "FFA"})
+	for cur.Next(ctx) {
+		var m Match
+		err := cur.Decode(&m)
+		if err != nil {
+			log.Printf("[ERROR] failed to collect data: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-	if err != nil {
-		log.Printf("[ERROR] getting total games, %s", err)
+		matches = append(matches, m)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Printf("[ERROR] failed to collect data: %s", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	string := fmt.Sprintf("Total games: %d", totalGames)
+	if err != nil {
+		log.Printf("[ERROR] failed to collect data: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-	_, _ = io.WriteString(w, string)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	err = json.NewEncoder(w).Encode(matches)
+	if err != nil {
+		log.Printf("[ERROR] failed to marshal data: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
--- app/api/types.go
+++ app/api/types.go
@@ -1,1 +1,40 @@
 package api
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type (
+	Match struct {
+		ID       primitive.ObjectID `json:"id" bson:"_id"`
+		Map      string             `json:"map"`
+		Type     string             `json:"type"`
+		Duration int                `json:"duration"`
+		Datetime string             `json:"datetime"`
+		Players  []Player           `json:"players"`
+	}
+
+	Player struct {
+		Name     string   `json:"name"`
+		Stats    []Stat   `json:"stats"`
+		Weapons  []Weapon `json:"weapons"`
+		Items    []Item   `json:"items"`
+		Powerups []Item   `json:"powerups"`
+	}
+
+	Stat struct {
+		Name  string `json:"name"`
+		Value int    `json:"value"`
+	}
+
+	Weapon struct {
+		Name  string `json:"name"`
+		Hits  int    `json:"hits"`
+		Shots int    `json:"shots"`
+		Kills int    `json:"kills"`
+	}
+
+	Item struct {
+		Name    string `json:"name"`
+		Pickups int    `json:"pickups"`
+		Time    int    `json:"time"`
+	}
+)
