fa5ff6fbaf5dfc65da28f45d9b6aceaa3c80940a
Alexander Dobromilskiy <alexdob.twist@gmail.com>
2022-09-18 20:03:26 +0300

    add tests

---
+++ app/api/server_test.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func TestFAAtotalsHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/api/ffa", nil)
+	w := httptest.NewRecorder()
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	db := client.Database("quake3")
+
+	srv := Server{
+		Port:     ":8080",
+		Database: db,
+	}
+	srv.getFAAtotals(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
+
+func TestFAAplayersHandler(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/api/ffa/players", nil)
+	w := httptest.NewRecorder()
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	db := client.Database("quake3")
+
+	srv := Server{
+		Port:     ":8080",
+		Database: db,
+	}
+	srv.getFAAplayers(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status OK; got %v", res.Status)
+	}
+}
